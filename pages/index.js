c5d3c4319aa1c8ee9b37322e85df93d120b625b9
Tin Ceraj <tin.ceraj@mindsmiths.com>
2021-05-17 23:25:55 +0200

    linking between sites

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -11,7 +12,13 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Test" />
+          <h1>
+              Learn{' '}
+              <Link href="/posts/first-post">
+                  <a>Next.js</a>
+              </Link>
+          </h1>
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
